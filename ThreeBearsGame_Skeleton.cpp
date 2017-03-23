09e2693be8a6fcfe0a02fc92e2a2c4a311e5c178
b6009240 <b6009240@my.shu.ac.uk>
2017-03-23 10:43:07 +0000

    B - Bears escaped

--- ThreeBearsGame_Skeleton.cpp
+++ ThreeBearsGame_Skeleton.cpp
@@ -460,16 +460,19 @@ void paintGame(const char g[][SIZEX], string mess, int noOfBears, int noOfMoves)
 	showMessage(clWhite, clRed, 40, 0, "FoP Task 1c: February 2017");
 	//Rescued
 	string bearString = "";
+	int bears(0);
 	for (int i = noOfBears; i < 3; i++)
 	{
 		bearString += "@";
+		bears++;
 	}
 	showMessage(clGrey, clYellow, 0, 1, "RESCUED " + bearString);
 
 	//display menu options available
 	showMessage(clRed, clYellow, 40, 3, "TO MOVE USE KEYBOARD ARROWS ");
 	showMessage(clRed, clYellow, 40, 4, "TO QUIT ENTER 'Q'           ");
-	showMessage(clBlack, clWhite, 40, 6, "NUMBER OF MOVES:" + to_string(noOfMoves));
+	showMessage(clBlack, clWhite, 40, 6, "NUMBER OF MOVES: " + to_string(noOfMoves));
+	showMessage(clBlack, clWhite, 40, 7, "BEARS ESCAPED:   " + to_string(bears));
 
 	//print auxiliary messages if any
 	showMessage(clBlack, clWhite, 40, 8, mess);	//display current message
